1911e8ad95c81c33e46c233d2c3a6b8fe84134c9
zhuaibing <zab995578513@qq.com>
2017-10-25 14:01:26 +0800

    行情

---
+++ app/src/main/java/com/honglu/future/ui/market/fragment/MarketFragment.java
@@ -0,0 +1,26 @@
+package com.honglu.future.ui.market.fragment;
+
+import com.honglu.future.base.BaseFragment;
+import com.honglu.future.ui.market.presenter.MarketPresenter;
+
+/**
+ * Created by zhuaibing on 2017/10/25
+ */
+
+public class MarketFragment extends BaseFragment<MarketPresenter>{
+
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+}
---
+++ app/src/main/java/com/honglu/future/ui/market/presenter/MarketPresenter.java
@@ -0,0 +1,12 @@
+package com.honglu.future.ui.market.presenter;
+
+import com.honglu.future.base.BasePresenter;
+import com.honglu.future.ui.market.contract.MarketContract;
+
+/**
+ * Created by zhuaibing on 2017/10/25
+ */
+
+public class MarketPresenter extends BasePresenter<MarketContract.View> implements MarketContract.Presenter{
+
+}
