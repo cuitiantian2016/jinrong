52f5dc4a25d58b9388488c66a978051867f4d45d
zhangqi <zhangqi@xnsudai.com>
2017-10-27 15:39:25 +0800

    交易说明popupWindow

--- app/src/main/java/com/honglu/future/ui/trade/fragment/OpenTransactionFragment.java
+++ app/src/main/java/com/honglu/future/ui/trade/fragment/OpenTransactionFragment.java
@@ -1,11 +1,19 @@
 package com.honglu.future.ui.trade.fragment;
 
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import com.honglu.future.R;
 import com.honglu.future.app.App;
@@ -32,10 +40,11 @@ import butterknife.OnClick;
 public class OpenTransactionFragment extends BaseFragment<OpenTransactionPresenter> implements OpenTransactionContract.View {
     @BindView(R.id.rv_open_transaction_list_view)
     RecyclerView mOpenTransactionListView;
-    LinearLayout mTradeHeader;
+    private LinearLayout mTradeHeader;
+    private ImageView mTradeTip;
     private OpenTransactionAdapter mOpenTransactionAdapter;
     private List<OpenTransactionListBean> mList;
-
+    private PopupWindow mPopupWindow;
 
     @Override
     public int getLayoutId() {
@@ -59,6 +68,7 @@ public class OpenTransactionFragment extends BaseFragment<OpenTransactionPresent
         mOpenTransactionAdapter = new OpenTransactionAdapter();
         View headView = LayoutInflater.from(mActivity).inflate(R.layout.item_trade_list_header, null);
         mTradeHeader = (LinearLayout) headView.findViewById(R.id.ll_trade_header);
+        mTradeTip = (ImageView) headView.findViewById(R.id.iv_trade_tip);
         mOpenTransactionAdapter.addHeaderView(headView);
         mOpenTransactionListView.setAdapter(mOpenTransactionAdapter);
         setListener();
@@ -72,6 +82,13 @@ public class OpenTransactionFragment extends BaseFragment<OpenTransactionPresent
                     Intent intent = new Intent(mActivity, LoginActivity.class);
                     startActivity(intent);
                 }
+            }
+        });
+
+        mTradeTip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomWindow(mTradeTip);
             }
         });
     }
@@ -102,4 +119,43 @@ public class OpenTransactionFragment extends BaseFragment<OpenTransactionPresent
         }
         mOpenTransactionAdapter.addData(mList);
     }
+
+    private void showBottomWindow(View view) {
+        if (mPopupWindow != null && mPopupWindow.isShowing()) {
+            return;
+        }
+        View headView = LayoutInflater.from(mActivity).inflate(R.layout.layout_trade_tip_pop_window, null);
+        mPopupWindow = new PopupWindow(headView,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+        //点击空白处时，隐藏掉pop窗口
+        mPopupWindow.setFocusable(true);
+        mPopupWindow.setBackgroundDrawable(new BitmapDrawable());
+        //添加弹出、弹入的动画
+        mPopupWindow.setAnimationStyle(R.style.Popupwindow);
+        int[] location = new int[2];
+        view.getLocationOnScreen(location);
+        mPopupWindow.showAtLocation(view, Gravity.LEFT | Gravity.BOTTOM, 0, -location[1]);
+        //添加按键事件监听
+        setButtonListeners(headView);
+        //添加pop窗口关闭事件，主要是实现关闭时改变背景的透明度
+        mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+            }
+        });
+    }
+
+    private void setButtonListeners(View view) {
+        ImageView ivClose = (ImageView) view.findViewById(R.id.iv_close_popup);
+        ivClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPopupWindow != null && mPopupWindow.isShowing()) {
+                    mPopupWindow.dismiss();
+                }
+            }
+        });
+    }
+
 }
