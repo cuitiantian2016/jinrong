231db40f4a1d5d98d03e351b2e392fd39e3139f0
zhuaibing <zab995578513@qq.com>
2017-10-27 11:23:04 +0800

    日期控件

--- app/src/main/java/com/honglu/future/ui/trade/activity/TradeRecordActivity.java
+++ app/src/main/java/com/honglu/future/ui/trade/activity/TradeRecordActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import com.honglu.future.R;
 import com.honglu.future.base.BaseActivity;
+import com.honglu.future.dialog.DateDialog;
 import com.honglu.future.ui.trade.adapter.TradeRecordAdapter;
 import com.honglu.future.ui.trade.presenter.TradeRecordPresenter;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
@@ -30,6 +31,12 @@ public class TradeRecordActivity extends BaseActivity<TradeRecordPresenter> impl
     @BindView(R.id.refreshView)
     SmartRefreshLayout refreshView;
 
+    private TextView tvStartTime;
+    private TextView tvEndTime;
+    private TextView tvRisk;
+    private TextView tvJiancang;
+    private TextView tvPingcang;
+    private TextView tvRevoke;
     private View tabJcLayout;
     private TextView tabJcText;
     private View tabJcLine;
@@ -41,6 +48,8 @@ public class TradeRecordActivity extends BaseActivity<TradeRecordPresenter> impl
     private View tabRLine;
 
     private TradeRecordAdapter mAdapter;
+    private DateDialog mDateDialog;
+
 
     @Override
     public int getLayoutId() {
@@ -54,7 +63,18 @@ public class TradeRecordActivity extends BaseActivity<TradeRecordPresenter> impl
 
     @Override
     public void loadData() {
+        mDateDialog = new DateDialog(TradeRecordActivity.this);
         View view = LayoutInflater.from(TradeRecordActivity.this).inflate(R.layout.layout_trade_record_top,null);
+        tvStartTime = (TextView) view.findViewById(R.id.tv_startTime);
+        tvEndTime = (TextView) view.findViewById(R.id.tv_endTime);
+        //风险率
+        tvRisk = (TextView) view.findViewById(R.id.tv_risk);
+        //建仓手数
+        tvJiancang = (TextView) view.findViewById(R.id.tv_jiancang);
+        //平仓手数
+        tvPingcang = (TextView) view.findViewById(R.id.tv_pingcang);
+        //已撤单手数
+        tvRevoke = (TextView) view.findViewById(R.id.tv_revoke);
         //建仓
         tabJcLayout = view.findViewById(R.id.tab_jcLayout);
         tabJcText = (TextView) view.findViewById(R.id.tab_jcText);
@@ -68,6 +88,8 @@ public class TradeRecordActivity extends BaseActivity<TradeRecordPresenter> impl
         tabRText = (TextView) view.findViewById(R.id.tab_rText);
         tabRLine = view.findViewById(R.id.tab_rLine);
 
+        tvStartTime.setOnClickListener(this);
+        tvEndTime.setOnClickListener(this);
         tabJcLayout.setOnClickListener(this);
         tabCcLayout.setOnClickListener(this);
         tabRLayout.setOnClickListener(this);
@@ -79,6 +101,15 @@ public class TradeRecordActivity extends BaseActivity<TradeRecordPresenter> impl
         }
         mAdapter = new TradeRecordAdapter(TradeRecordActivity.this,list);
         lvListView.setAdapter(mAdapter);
+
+        mDateDialog.setBirthdayListener(new DateDialog.OnBirthListener() {
+            @Override
+            public void onClick(String start, String end) {
+                tvStartTime.setText(start);
+                tvEndTime.setText(end);
+                mDateDialog.dismiss();
+            }
+        });
     }
 
 
@@ -86,6 +117,12 @@ public class TradeRecordActivity extends BaseActivity<TradeRecordPresenter> impl
     @Override
     public void onClick(View v) {
       switch (v.getId()){
+          case R.id.tv_startTime:
+              mDateDialog.show();
+              break;
+          case R.id.tv_endTime:
+              mDateDialog.show();
+              break;
           case R.id.tab_jcLayout:
               tabJcText.setTextColor(getResources().getColor(R.color.color_008EFF));
               tabJcLine.setBackgroundResource(R.color.color_008EFF);
