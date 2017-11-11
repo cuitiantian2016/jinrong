92ab91c1a2fb350627ac1ac7fdf17818a1764096
zhuaibing <zab995578513@qq.com>
2017-11-11 15:26:58 +0800

    按钮底色

--- app/src/main/java/com/honglu/future/ui/market/activity/OptionalQuotesActivity.java
+++ app/src/main/java/com/honglu/future/ui/market/activity/OptionalQuotesActivity.java
@@ -1,11 +1,13 @@
 package com.honglu.future.ui.market.activity;
 
 import android.app.Service;
+import android.graphics.Color;
 import android.os.Vibrator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -238,6 +240,7 @@ public class OptionalQuotesActivity extends BaseActivity<OptionalQuotesPresenter
 
             @Override
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                  Log.d("wahcc","====onSwiped=====");
 //                int position = viewHolder.getAdapterPosition();
 //                myAdapter.notifyItemRemoved(position);
 //                datas.remove(position);
@@ -260,9 +263,9 @@ public class OptionalQuotesActivity extends BaseActivity<OptionalQuotesPresenter
              */
             @Override
             public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
-//                if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
-//                    viewHolder.itemView.setBackgroundColor(Color.LTGRAY);
-//                }
+                if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+                    viewHolder.itemView.setBackgroundColor(Color.TRANSPARENT);
+                }
                 super.onSelectedChanged(viewHolder, actionState);
             }
 
@@ -274,7 +277,7 @@ public class OptionalQuotesActivity extends BaseActivity<OptionalQuotesPresenter
             @Override
             public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
                 super.clearView(recyclerView, viewHolder);
-                //viewHolder.itemView.setBackgroundColor(0);
+                viewHolder.itemView.setBackgroundColor(0);
                 //saveMarketData(zxAdapter !=null ? zxAdapter.getData() : null);
             }
         });
