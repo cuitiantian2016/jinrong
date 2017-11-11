2ec1da57097d30e2121f86a39c2cc459e58bb286
zhangqi <zhangqi@xnsudai.com>
2017-11-11 19:37:12 +0800

    添加引导页

--- app/src/main/java/com/honglu/future/ui/main/activity/SplashActivity.java
+++ app/src/main/java/com/honglu/future/ui/main/activity/SplashActivity.java
@@ -5,14 +5,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.provider.Settings;
 import android.widget.RelativeLayout;
+
 import com.honglu.future.R;
 import com.honglu.future.app.App;
 import com.honglu.future.base.BaseActivity;
 import com.honglu.future.base.PermissionsListener;
+import com.honglu.future.config.Constant;
 import com.honglu.future.dialog.AlertFragmentDialog;
 import com.honglu.future.events.LoginNoRefreshUIEvent;
 import com.honglu.future.ui.main.contract.SplashContract;
+import com.honglu.future.ui.main.guide.GuideActivity;
 import com.honglu.future.ui.main.presenter.SplashPresenter;
+import com.honglu.future.util.SpUtil;
 import com.tencent.android.tpush.XGPushClickedResult;
 import com.tencent.android.tpush.XGPushManager;
 
@@ -26,13 +30,14 @@ import butterknife.BindView;
  * 启动页
  * xiejingwen
  */
-public class SplashActivity extends BaseActivity<SplashPresenter> implements SplashContract.View{
+public class SplashActivity extends BaseActivity<SplashPresenter> implements SplashContract.View {
 
     @BindView(R.id.rl_splash)
     RelativeLayout mRlSplash;
     private boolean isRequesting;//为了避免在onResume中多次请求权限
     private String[] permissions = {Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE
-            , Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.READ_CONTACTS};
+            , Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.READ_CONTACTS};
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_splash;
@@ -70,36 +75,35 @@ public class SplashActivity extends BaseActivity<SplashPresenter> implements Spl
             return;
         }
     }
+
     @Override
     protected void onResume() {
         super.onResume();
-        if (!isRequesting) {
-            mRlSplash.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    if (App.getConfig().isDebug()){
-                        startActivity(MainActivity.class);
-                        //startActivity(UrlSelectorActivity.class);
-                    }else{
-                        startActivity(MainActivity.class);
-                        //为了测试修改过
-                        //startActivity(UrlSelectorActivity.class);
-                    }
+        //if (!isRequesting) {
+        mRlSplash.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (SpUtil.getInt(Constant.CACHE_IS_FIRST_LOGIN, Constant.HAS_ALREADY_LOGIN) == Constant.HAS_ALREADY_LOGIN) {
+                    startActivity(GuideActivity.class);
                     finish();
-                    //requestPermissions(permissions, mListener);
+                } else {
+                    startActivity(MainActivity.class);
+                    finish();
                 }
-            },800);
-            isRequesting = true;
-        }
+            }
+        }, 800);
+        //  isRequesting = true;
+        //}
     }
+
     private PermissionsListener mListener = new PermissionsListener() {
         @Override
         public void onGranted() {
             isRequesting = false;
-            if (App.getConfig().isDebug()){
+            if (App.getConfig().isDebug()) {
                 startActivity(MainActivity.class);
                 //startActivity(UrlSelectorActivity.class);
-            }else{
+            } else {
                 startActivity(MainActivity.class);
                 //为了测试修改过
                 //startActivity(UrlSelectorActivity.class);
@@ -120,6 +124,7 @@ public class SplashActivity extends BaseActivity<SplashPresenter> implements Spl
             }*/
             EventBus.getDefault().post(new LoginNoRefreshUIEvent(getApplicationContext(), App.getConfig().getUserInfo()));
         }
+
         @Override
         public void onDenied(List<String> deniedPermissions, boolean isNeverAsk) {
             if (!isNeverAsk) {//请求权限没有全被勾选不再提示然后拒绝
@@ -143,7 +148,7 @@ public class SplashActivity extends BaseActivity<SplashPresenter> implements Spl
                     public void dialogLeftBtnClick() {
                         finish();
                     }
-                }).setContent("\""+ App.getAPPName()+"\"缺少必要权限\n请手动授予\""+ App.getAPPName()+"\"访问您的权限")
+                }).setContent("\"" + App.getAPPName() + "\"缺少必要权限\n请手动授予\"" + App.getAPPName() + "\"访问您的权限")
                         .setRightBtnText("授权").setRightCallBack(new AlertFragmentDialog.RightClickCallBack() {
                     @Override
                     public void dialogRightBtnClick(String string) {
