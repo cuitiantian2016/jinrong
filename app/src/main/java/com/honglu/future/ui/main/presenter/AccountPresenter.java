040356e038a54476c0db3607892300e6e0caa767
hefei <mingdong.fang@yiquds.com>
2017-11-15 11:24:20 +0800

    [WN] 首次登录添加弹窗提示

--- app/src/main/java/com/honglu/future/ui/main/presenter/AccountPresenter.java
+++ app/src/main/java/com/honglu/future/ui/main/presenter/AccountPresenter.java
@@ -1,10 +1,13 @@
 package com.honglu.future.ui.main.presenter;
 
+import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
 
+import com.honglu.future.R;
 import com.honglu.future.app.App;
 import com.honglu.future.base.BasePresenter;
 import com.honglu.future.config.Constant;
+import com.honglu.future.dialog.AlertFragmentDialog;
 import com.honglu.future.http.HttpManager;
 import com.honglu.future.http.HttpSubscriber;
 import com.honglu.future.ui.main.contract.AccountContract;
@@ -17,7 +20,7 @@ import com.honglu.future.util.SpUtil;
  * Created by zq on 2017/11/3.
  */
 
-public class AccountPresenter extends BasePresenter<AccountContract.View> implements AccountContract.Presenter{
+public class AccountPresenter extends BasePresenter<AccountContract.View> implements AccountContract.Presenter {
     @Override
     public void login(final String account, String password, String userId, String company, final TextView tv_pass) {
         toSubscribe(HttpManager.getApi().loginAccount(account, AESUtils.encrypt(password), userId, company), new HttpSubscriber<AccountBean>() {
@@ -36,11 +39,19 @@ public class AccountPresenter extends BasePresenter<AccountContract.View> implem
             @Override
             protected void _onError(String message, int code) {
                 super._onError(message, code);
-                mView.showErrorMsg(message, null);
-                if (code == 70000){//首次登录
+                if (code == 70000) {//首次登录
                     tv_pass.setText("");
                     SpUtil.putString(Constant.CACHE_ACCOUNT_USER_NAME, account);
-                    PasswordResetActivity.startPasswordResetActivity(App.mApp.mActivity,true);
+                    new AlertFragmentDialog.Builder((FragmentActivity) App.mApp.mActivity)
+                            .setLeftBtnText("取消").setContent(message, R.color.color_333333, R.dimen.dimen_25sp).setTitle("", R.color.color_3C383F, R.dimen.dimen_16sp)
+                            .setRightBtnText("确定").setRightCallBack(new AlertFragmentDialog.RightClickCallBack() {
+                        @Override
+                        public void dialogRightBtnClick(String string) {
+                            PasswordResetActivity.startPasswordResetActivity(App.mApp.mActivity, true);
+                        }
+                    }).create(AlertFragmentDialog.Builder.TYPE_NORMAL);
+                } else {
+                    mView.showErrorMsg(message, null);
                 }
             }
 
