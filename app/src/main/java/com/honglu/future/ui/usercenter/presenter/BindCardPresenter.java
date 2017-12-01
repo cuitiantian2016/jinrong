b5972a1e03a5ec0b2b971c703b2d1a278baf95d0
zhuaibing <zab995578513@qq.com>
2017-12-01 20:12:06 +0800

    ui还原

--- app/src/main/java/com/honglu/future/ui/usercenter/presenter/BindCardPresenter.java
+++ app/src/main/java/com/honglu/future/ui/usercenter/presenter/BindCardPresenter.java
@@ -6,6 +6,7 @@ import com.honglu.future.http.HttpSubscriber;
 import com.honglu.future.ui.usercenter.bean.BindCardBean;
 import com.honglu.future.ui.usercenter.contract.BindCardContract;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,23 +17,33 @@ public class BindCardPresenter extends BasePresenter<BindCardContract.View> impl
 
     @Override
     public void getBindCardInfo(String userId, String token) {
-        toSubscribe(HttpManager.getApi().geBindCardData(userId, token), new HttpSubscriber<List<BindCardBean>>() {
-            @Override
-            protected void _onStart() {
-                mView.showLoading("获取中...");
-            }
+//        toSubscribe(HttpManager.getApi().geBindCardData(userId, token), new HttpSubscriber<List<BindCardBean>>() {
+//            @Override
+//            protected void _onStart() {
+//                mView.showLoading("获取中...");
+//            }
+//
+//            @Override
+//            protected void _onError(String message) {
+//                mView.stopLoading();
+//                mView.showErrorMsg(message, null);
+//            }
+//
+//            @Override
+//            protected void _onNext(List<BindCardBean> bindCardBeen) {
+//                mView.stopLoading();
+//                mView.getBindCardInfo(bindCardBeen);
+//            }
+//        });
 
-            @Override
-            protected void _onError(String message) {
-                mView.stopLoading();
-                mView.showErrorMsg(message, null);
-            }
-
-            @Override
-            protected void _onNext(List<BindCardBean> bindCardBeen) {
-                mView.stopLoading();
-                mView.getBindCardInfo(bindCardBeen);
-            }
-        });
+        List<BindCardBean> list = new ArrayList<>();
+        for (int i = 0 ; i < 8 ; i ++){
+            BindCardBean bean = new BindCardBean();
+            bean.setBankName("测试银行");
+            bean.setBankAccount("622123456784761");
+            bean.setBankId(i+1+"");
+            list.add(bean);
+        }
+        mView.getBindCardInfo(list);
     }
 }
