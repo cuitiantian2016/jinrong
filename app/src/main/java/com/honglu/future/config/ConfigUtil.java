bb01ada1a73d70b7be22fdbd0b43cf7c0714dd49
zhangqi <zhangqi@xnsudai.com>
2017-12-20 13:34:06 +0800

    开户链接和关于我们增加版本号参数

--- app/src/main/java/com/honglu/future/config/ConfigUtil.java
+++ app/src/main/java/com/honglu/future/config/ConfigUtil.java
@@ -7,6 +7,7 @@ import android.text.TextUtils;
 
 import com.honglu.future.BuildConfig;
 import com.honglu.future.ui.usercenter.bean.UserInfoBean;
+import com.honglu.future.util.AppUtils;
 import com.honglu.future.util.ConvertUtil;
 import com.honglu.future.util.SpUtil;
 import com.orhanobut.logger.Logger;
@@ -40,7 +41,7 @@ public class ConfigUtil {
     public static String NEW_USER_TEACH = baseH5Url + "newSchool";
 
     //关于我们
-    public static String ABOUT_US = baseH5Url + "aboutUs";
+    public static String ABOUT_US = baseH5Url + "aboutUs?version="+ AppUtils.getVersionName();
     //出入金规则
     public static String SCHOOLDETAILS = baseH5Url + "schoolDetails/14";
 
@@ -48,7 +49,7 @@ public class ConfigUtil {
     public static String TRADE_RULE = baseH5Url + "rule";
 
     //开户入口
-    public static String OPEN_ACCOUNT_HOME = baseH5Url + "initAccount";
+    public static String OPEN_ACCOUNT_HOME = baseH5Url + "initAccount?version="+ AppUtils.getVersionName();
 
     //绑卡教程
     public static String BIND_CARD_TEACH = baseH5Url + "addCard";
