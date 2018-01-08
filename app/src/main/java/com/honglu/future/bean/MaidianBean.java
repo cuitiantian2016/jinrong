a2c0aa5639258e038d3e447c8a922c5bc0c6c804
hefei <mingdong.fang@yiquds.com>
2018-01-08 15:16:48 +0800

    [wn] 埋点点击数量

--- app/src/main/java/com/honglu/future/bean/MaidianBean.java
+++ app/src/main/java/com/honglu/future/bean/MaidianBean.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonNull;
+import com.google.gson.annotations.SerializedName;
 import com.honglu.future.app.App;
 import com.honglu.future.config.Constant;
 import com.honglu.future.http.HttpManager;
@@ -21,28 +22,28 @@ import rx.schedulers.Schedulers;
 
 /**
  * Created by hefei on 2017/12/29
- *
+ * <p>
  * 埋点的数据结构
- *
- *     {
- "uuid":"7uhajkduqwo398xjdjakdkDGf",
- "product_name":"qihuo",
- "message_id":"200010",
- "user_id":"13918405190",
- "page_name":"激活",
- "even_name":"激活",
- "data":{
- "buriedName":"激活",
- "buriedRemark":"首次下载并打开APP的用户    注册日期相同的首次下载并打开APP的用户人数合计",
- "clickNum": 10,
- "deviceId" :"TYUIOYUIKL" //设备编号
- "key": "jihuo_shoucidakai",
- "userId" : "JLSDFKDJFLSDFKJSLDF"
- }
- }
+ * <p>
+ * {
+ * "uuid":"7uhajkduqwo398xjdjakdkDGf",
+ * "product_name":"qihuo",
+ * "message_id":"200010",
+ * "user_id":"13918405190",
+ * "page_name":"激活",
+ * "even_name":"激活",
+ * "data":{
+ * "buriedName":"激活",
+ * "buriedRemark":"首次下载并打开APP的用户    注册日期相同的首次下载并打开APP的用户人数合计",
+ * "clickNum": 10,
+ * "deviceId" :"TYUIOYUIKL" //设备编号
+ * "key": "jihuo_shoucidakai",
+ * "userId" : "JLSDFKDJFLSDFKJSLDF"
+ * }
+ * }
  */
 
-public class MaidianBean implements Serializable{
+public class MaidianBean implements Serializable {
 
     public String uuid = UUID.randomUUID().toString();
     public String product_name = "qihuo";
@@ -52,25 +53,26 @@ public class MaidianBean implements Serializable{
     public String even_name;
     public Data data;
 
-    public static class Data{
+    public static class Data {
         public String buriedName;
         public String buriedRemark;
         public String deviceId = ViewUtil.getDeviceId(App.getContext());
         public String key;
-        public String company ;
+        public String company;
+        public int clickNum = 1;//埋点点击数量默认是1.需要时候++
         public String mobile = SpUtil.getString(Constant.CACHE_TAG_MOBILE);
 
     }
 
 
-    public static void postMaiDian(MaidianBean maidianBean){
-        if (maidianBean==null){
+    public static void postMaiDian(MaidianBean maidianBean) {
+        if (maidianBean == null) {
             return;
         }
         Gson gson = new Gson();
-        String route= gson.toJson(maidianBean);//通过Gson将Bean转化为Json字符串形式
-        Log.d("MaidianBean", "initPresenter: route--->"+route);
-        RequestBody body= RequestBody.create(MediaType.parse("application/json; charset=utf-8"),route);
+        String route = gson.toJson(maidianBean);//通过Gson将Bean转化为Json字符串形式
+        Log.d("MaidianBean", "initPresenter: route--->" + route);
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), route);
         HttpManager.getApi().postMaiDian(body).subscribeOn(Schedulers.io()).subscribe(new Subscriber<MaidianReturn>() {
             @Override
             public void onCompleted() {
@@ -84,7 +86,7 @@ public class MaidianBean implements Serializable{
 
             @Override
             public void onNext(MaidianReturn data) {
-                Log.d("MaidianBean", "onNext:--》 "+data);
+                Log.d("MaidianBean", "onNext:--》 " + data);
             }
         });
     }
