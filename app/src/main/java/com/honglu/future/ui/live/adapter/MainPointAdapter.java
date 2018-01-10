fb1cc4480b01668e81d16b14239e147b44fbb106
zhangqi <zhangqi@xnsudai.com>
2018-01-10 20:23:56 +0800

    还原度调整

--- app/src/main/java/com/honglu/future/ui/live/adapter/MainPointAdapter.java
+++ app/src/main/java/com/honglu/future/ui/live/adapter/MainPointAdapter.java
@@ -1,8 +1,11 @@
 package com.honglu.future.ui.live.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.honglu.future.R;
@@ -26,10 +29,46 @@ public class MainPointAdapter extends BaseRecyclerAdapter<MainPointAdapter.ViewH
     }
 
     @Override
-    public void mOnBindViewHolder(MainPointAdapter.ViewHolder holder, final int position) {
-        holder.mTvContent.setText(item.messageDescribe);
-        holder.mTvTitle.setText(item.messageTitle);
+    public void mOnBindViewHolder(final MainPointAdapter.ViewHolder holder, final int position) {
+
         holder.mTvTime.setText(item.createTime);
+
+        if (item.messageTitle.length() > 14) {
+            holder.mTvTitle.setText(item.messageTitle.substring(0, 13) + "...");
+        } else {
+            holder.mTvTitle.setText(item.messageTitle);
+        }
+        holder.mTvContent.setText(item.messageDescribe);
+        holder.mTvContentAll.setText(item.messageDescribe);
+        holder.isShow = false;
+        if (item.messageDescribe.length() > 100) {
+            holder.mTvContent.setVisibility(View.VISIBLE);
+            holder.mTvContentAll.setVisibility(View.GONE);
+            holder.mIvArrow.setVisibility(View.VISIBLE);
+        } else {
+            holder.mTvContent.setVisibility(View.GONE);
+            holder.mTvContentAll.setVisibility(View.VISIBLE);
+            holder.mIvArrow.setVisibility(View.GONE);
+        }
+
+        holder.mIvArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!holder.isShow) {
+                    holder.isShow = true;
+                    holder.mIvArrow.setImageResource(R.mipmap.ic_up);
+                    holder.mTvContent.setVisibility(View.GONE);
+                    holder.mTvContentAll.setVisibility(View.VISIBLE);
+                } else {
+                    holder.isShow = false;
+                    holder.mIvArrow.setImageResource(R.mipmap.ic_down);
+                    holder.mTvContent.setVisibility(View.VISIBLE);
+                    holder.mTvContentAll.setVisibility(View.GONE);
+                }
+
+            }
+        });
+
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
@@ -39,6 +78,12 @@ public class MainPointAdapter extends BaseRecyclerAdapter<MainPointAdapter.ViewH
         TextView mTvTime;
         @BindView(R.id.tv_title)
         TextView mTvTitle;
+        @BindView(R.id.tv_content_all)
+        TextView mTvContentAll;
+        @BindView(R.id.iv_arrow)
+        ImageView mIvArrow;
+
+        boolean isShow;
 
         ViewHolder(View view) {
             super(view);
