b6c3066a4fe46da6efaac642750bdfa789747f0d
hefei <mingdong.fang@yiquds.com>
2018-01-11 16:30:26 +0800

    [wn] 首页ui修改

--- app/src/main/java/com/honglu/future/ui/home/viewmodel/HomeBottomTabViewModel.java
+++ app/src/main/java/com/honglu/future/ui/home/viewmodel/HomeBottomTabViewModel.java
@@ -27,13 +27,14 @@ import java.util.ArrayList;
 public class HomeBottomTabViewModel {
 
     private CommonTabLayout mCommonTabLayout;
-    private ArrayList<Fragment> mFragments ;
+//    private ArrayList<Fragment> mFragments ;
     private int currentPosition;
-    private NewsColumnFragment newsColumnFragment;
-    private NewsFlashFragment newsFlashFragment;
+//    private NewsColumnFragment newsColumnFragment;
+//    private NewsFlashFragment newsFlashFragment;
     private SmartRefreshLayout mSmartRefreshLayout;
     private Context mContext;
     public final View mView;
+    private NewsColumnFragment newsColumnFragment;
 
 
     public HomeBottomTabViewModel(Context context, SmartRefreshLayout smartRefreshLayout) {
@@ -44,54 +45,57 @@ public class HomeBottomTabViewModel {
     }
 
     public void initView(View view) {
-        mCommonTabLayout = (CommonTabLayout) view.findViewById(R.id.home_common_tab_layout);
+//        mCommonTabLayout = (CommonTabLayout) view.findViewById(R.id.home_common_tab_layout);
         //int screenWidthDip = DeviceUtils.px2dip(mContext, DeviceUtils.getScreenWidth(mContext));
          //mCommonTabLayout.setIndicatorWidth((int) (screenWidthDip*0.2f));
-        ArrayList<CustomTabEntity> entities = new ArrayList<>();
-        entities.add(new TabEntity(mContext.getString(R.string.news_column)));
-        entities.add(new TabEntity(mContext.getString(R.string.news_flash)));
-        if (mFragments == null){
-            mFragments = new ArrayList<>();
-        }
-        newsColumnFragment = new NewsColumnFragment();//海豚专栏
-        mFragments.add(newsColumnFragment);
-        newsFlashFragment = new NewsFlashFragment();//24小时
-        mFragments.add(newsFlashFragment);
-        mCommonTabLayout.setTabData(entities,(FragmentActivity) mContext,R.id.fragment_container_id,mFragments);
-        mCommonTabLayout.setOnTabSelectListener(new SimpleOnTabSelectListener(){
-            @Override
-            public void onTabSelect(int position) {
-                super.onTabSelect(position);
-                mSmartRefreshLayout.finishLoadmore();//每次切换的时候不显示上一个刷新
-                mSmartRefreshLayout.finishRefresh();
-                currentPosition = position;
-                if (currentPosition == 0){
-                    mSmartRefreshLayout.setEnableLoadmore(false);
-                }else {
-                    MobclickAgent.onEvent(mContext,"shouye_24hkuaixun_click","首页_24h快讯");
-                    mSmartRefreshLayout.setEnableLoadmore(true);
-                }
-                refreshData();
-            }
-        });
-        mSmartRefreshLayout.setEnableLoadmore(false);
+//        ArrayList<CustomTabEntity> entities = new ArrayList<>();
+//        entities.add(new TabEntity(mContext.getString(R.string.news_column)));
+        //entities.add(new TabEntity(mContext.getString(R.string.news_flash)));
+//        if (mFragments == null){
+//            mFragments = new ArrayList<>();
+//        }
+//        newsColumnFragment =//海豚专栏
+//        mFragments.add(newsColumnFragment);
+//        newsFlashFragment = new NewsFlashFragment();//24小时
+//        mFragments.add(newsFlashFragment);
+//        mCommonTabLayout.setTabData(entities,(FragmentActivity) mContext,R.id.fragment_container_id,mFragments);
+//        mCommonTabLayout.setOnTabSelectListener(new SimpleOnTabSelectListener(){
+//            @Override
+//            public void onTabSelect(int position) {
+//                super.onTabSelect(position);
+//                mSmartRefreshLayout.finishLoadmore();//每次切换的时候不显示上一个刷新
+//                mSmartRefreshLayout.finishRefresh();
+//                currentPosition = position;
+//                if (currentPosition == 0){
+//                    mSmartRefreshLayout.setEnableLoadmore(false);
+//                }else {
+//                    MobclickAgent.onEvent(mContext,"shouye_24hkuaixun_click","首页_24h快讯");
+//                    mSmartRefreshLayout.setEnableLoadmore(true);
+//                }
+//                refreshData();
+//            }
+//        });
+        newsColumnFragment = new NewsColumnFragment();
+      mSmartRefreshLayout.setEnableLoadmore(false);
+        ((FragmentActivity) mContext).getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_id,newsColumnFragment).commit();
     }
     /**
      * 下拉刷新
      */
     public void refreshData(){
-      if (currentPosition == 0){
-            newsColumnFragment.refresh();
-        }else if (currentPosition == 1){
-            newsFlashFragment.refresh();
-        }
+        newsColumnFragment.refresh();
+//      if (currentPosition == 0){
+//
+//        }else if (currentPosition == 1){
+////            newsFlashFragment.refresh();
+//        }
     }
     /**
      * 上拉加载更多
      */
     public void loadMore(){
-        if (currentPosition == 1){
-           newsFlashFragment.loadMore();
-        }
+//        if (currentPosition == 1){
+////           newsFlashFragment.loadMore();
+//        }
     }
 }
